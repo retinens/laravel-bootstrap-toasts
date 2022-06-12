9bd00885038e46a0d7cc453cd5e47d91e7efff3e
WhereIsLucas <lucas@retinens.com>
2022-06-12 22:46:26 +0200

    first commit

--- resources/views/message.blade.php
+++ resources/views/message.blade.php
@@ -2,17 +2,29 @@
     <div class="toast-container position-static">
         <div class="p-3 {{ $xPosition }}-0 {{ $yPosition }}-0">
             @foreach (session('toasts', collect())->toArray() as $toast)
-                <div class="toast" role="alert" aria-live="assertive" aria-atomic="true" data-bs-delay="5000" @if (!$autoHide) data-autohide="false" @endif>
-                    <div class="toast-header">
-                        @if (config('laravel-bootstrap-toasts.icon',null))
-                            <img src="{{ config('laravel-bootstrap-toasts.icon',null) }}" class="rounded me-2" alt="...">
-                        @endif
-                        <strong class="me-auto">{{ $toast['title'] ?? '' }}</strong>
-                        <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-                    </div>
-                    <div class="toast-body">
-                        {!! $toast['message'] !!}
-                    </div>
+                <div class="toast text-bg-{{ $toast['level'] ?? 'primary' }}" role="alert" aria-live="assertive" aria-atomic="true" data-bs-delay="5000"
+                     @if (!$autoHide) data-autohide="false" @endif>
+                    @if ($toast['title'])
+                        <div class="toast-header">
+                            @if (config('laravel-bootstrap-toasts.icon',null))
+                                <img src="{{ config('laravel-bootstrap-toasts.icon',null) }}" class="rounded me-2"
+                                     alt="...">
+                            @endif
+                            <strong class="me-auto">{{ $toast['title'] ?? '' }}</strong>
+                            <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+                        </div>
+                        <div class="toast-body">
+                            {!! $toast['message'] !!}
+                        </div>
+                    @else
+                        <div class="d-flex">
+                            <div class="toast-body">
+                                {!! $toast['message'] !!}
+                            </div>
+                            <button type="button" class="btn-close me-2 m-auto" data-bs-dismiss="toast"
+                                    aria-label="Close"></button>
+                        </div>
+                    @endif
                 </div>
             @endforeach
         </div>
@@ -21,7 +33,9 @@
     <script>
         const toastElList = document.querySelectorAll('.toast')
         const toastList = [...toastElList].map(toastEl => new bootstrap.Toast(toastEl))
-        toastList.forEach(function(item){item.show()})
+        toastList.forEach(function (item) {
+            item.show()
+        })
     </script>
     @php
         session()->forget('toasts')
